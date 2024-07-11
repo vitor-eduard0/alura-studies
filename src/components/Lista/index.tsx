76ed2e2ccce92ab3139283aa1b575e212a1e9840
vitor-eduard0 <vitoreduardo.dev@gmail.com>
2024-07-11 15:37:37 -0300

    Commit inicial.

---
+++ src/components/Lista/index.tsx
@@ -0,0 +1,23 @@
+import style from './Lista.module.scss';
+import Item from './Item';
+import { ITarefa } from '../../types/Tarefa';
+
+function Lista({ tarefas }: { tarefas: ITarefa[] }) {
+    return (
+        <aside className={style.listaTarefas}>
+            <h2>Estudos do dia</h2>
+            <ul>
+                {
+                    tarefas.map((item, index) => (
+                        <Item
+                            key={index}
+                            {...item}
+                        />
+                    ))
+                }
+            </ul>
+        </aside>
+    )
+}
+
+export default Lista;
---
+++ src/pages/App.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import Formulario from '../components/Formulario';
+import Lista from '../components/Lista';
+import style from './App.module.scss';
+import Cronometro from '../components/Cronometro';
+import { ITarefa } from '../types/Tarefa';
+
+function App() {
+  const [tarefas, setTarefas] = useState<ITarefa[] | []>([]);
+  return (
+    <div className={style.AppStyle}>
+      <Formulario setTarefas={setTarefas} />
+      <Lista tarefas={tarefas} />
+      <Cronometro />
+    </div>
+  );
+}
+
+export default App;
